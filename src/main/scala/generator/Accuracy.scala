0944f227eb474cc8b622c3429c7b16628aaeb0f1
孤月 <guyue.hzy@alibaba-inc.com>
2019-06-26 11:37:01 +0800

    fix Accuracy

--- src/main/scala/generator/Accuracy.scala
+++ src/main/scala/generator/Accuracy.scala
@@ -19,7 +19,7 @@ object Accuracy {
     val actual = Source.fromFile(actualFile).getLines().map(line => {
       val strs = line.split("""\^\^""")
       assert(strs.length == 3, s"illegal $strs")
-      (strs(0), if (Integer.valueOf(strs(1)) >= 2) 1 else 0)
+      (strs(0), if (strs(1).toInt >= 2) 1 else 0)
     })
 
     val predict = Source.fromFile(predictFile).getLines().map(line => {
@@ -30,6 +30,8 @@ object Accuracy {
     var count = 0
     var hit = 0
     var miss = 0
+    var zero = 0
+    var one = 0
     actual.zip(predict).foreach(item => {
       assert(item._1._1 == item._2._1)
       if (item._1._2 == item._2._2) {
@@ -37,8 +39,16 @@ object Accuracy {
       } else {
         miss += 1
       }
+
+      if(item._1._2 == 0){
+        zero += 1
+      }else{
+        one +=1
+      }
+
       count += 1
     })
     println(s"count: $count hit: $hit miss: $miss  accuracy: ${hit/count.asInstanceOf[Double]}")
+    println(s"JsonPath can cache: $one   JsonPath can't cache: $zero  ratio: ${one/count.asInstanceOf[Double]}")
   }
 }
